333973addfce67e7e2a6529ef43e566b250214a5
FrancescoZedde <francescozedde711@gmail.com>
2023-04-11 22:34:35 +0200

    commit user agent mobile

--- xzshop_sandbox/settings.py
+++ xzshop_sandbox/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    #'127.0.0.1',
     'sellfast.app',
     'www.sellfast.app',
     '139.144.179.11',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
     'users',
     'crispy_forms',
     "crispy_bootstrap5",
+    'django_user_agents',
 ]
 
 AUTH_USER_MODEL = 'users.CustomUser'
@@ -66,6 +68,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MIDDLEWARE_CLASSES = ('django_user_agents.middleware.UserAgentMiddleware')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'sellfast.app'}
+}
 
 ROOT_URLCONF = 'xzshop_sandbox.urls'
 
@@ -84,6 +94,10 @@ TEMPLATES = [
         },
     },
 ]
+
+
+TEMPLATE_LOADERS = 'django_mobile.loader.Loader',
+TEMPLATE_CONTEXT_PROCESSORS =  'django_mobile.context_processors.flavour',
 
 WSGI_APPLICATION = 'xzshop_sandbox.wsgi.application'
 
