199f7889e30177304e2343764f25facbed035bc7
FrancescoZedde <francescozedde711@gmail.com>
2023-04-28 23:55:08 +0200

    fix frontend

--- users/forms.py
+++ users/forms.py
@@ -44,10 +44,16 @@ class WhitelistForm(forms.Form):
 	email_address = forms.EmailField(max_length = 150)
 
 class ContactForm(forms.Form):
-	first_name = forms.CharField(max_length = 50)
-	last_name = forms.CharField(max_length = 50)
-	email_address = forms.EmailField(max_length = 150)
-	message = forms.CharField(widget = forms.Textarea, max_length = 2000)
+
+    options = (("account","Account related problems"),
+    ("feature","Request feature"),
+    ("payment","Billing"),
+    ("bug","Report bug"),
+    ("other","Other"))
+
+    email_address = forms.EmailField(max_length = 150)
+    problem = forms.TypedChoiceField(label='',choices=options, required=True)
+    message = forms.CharField(widget = forms.Textarea, max_length = 2000)
 
 class WooCommerceConnectForm(forms.Form):
     woocommerce_store_name = forms.CharField(max_length = 30, required=True, help_text='store name')
